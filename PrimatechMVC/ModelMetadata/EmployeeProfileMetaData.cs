f4e43c8e82b4e1d593859bb61e6e3f11d9fecb12
Michael Ibarra <bm2yogi@gmail.com>
2011-10-27 20:35:14 -0700

    Added a header, navbar and styling to master page.

--- PrimatechMVC/ModelMetadata/EmployeeProfileMetaData.cs
+++ PrimatechMVC/ModelMetadata/EmployeeProfileMetaData.cs
@@ -9,17 +9,18 @@ namespace PrimatechMVC.ModelMetadata
         [Display(ResourceType =typeof(Localization.Employees), Name="EmployeeId")]
         [GreaterThan(100, ErrorMessageResourceType=typeof(Localization.Validation), ErrorMessageResourceName = "EmployeeNumberTooSmall")]
         public int EmployeeId { get; set; }
-        
-        [StringLength(5, MinimumLength = 3)]
         public string FirstName { get; set; }
-        //[Display(ResourceType=typeof(Localization.Employees), Name = "LastName")]
-        //[StringLength(5, MinimumLength = 3, ErrorMessageResourceType = typeof(Localization.Validation), ErrorMessageResourceName = "StringLengthNotInRange")]
+        [Display(ResourceType = typeof(Localization.Employees), Name = "LastName")]
+        [StringLength(5, MinimumLength = 3, ErrorMessageResourceType = typeof(Localization.Validation), ErrorMessageResourceName = "StringLengthNotInRange")]
         public string LastName { get; set; }
         public string Position { get; set; }
         public string Department { get; set; }
+        
+        [DataType(DataType.Date)]
         public string HireDate { get; set; }
-        //[Required(ErrorMessageResourceType = typeof(Localization.Validation), ErrorMessageResourceName = "EmailAddressIsRequired")]
-        //[Email(ErrorMessageResourceType = typeof(Localization.Validation), ErrorMessageResourceName = "EmailAddressInvalidFormat")]
+        
+        [Required(ErrorMessageResourceType = typeof(Localization.Validation), ErrorMessageResourceName = "EmailAddressIsRequired")]
+        [Email(ErrorMessageResourceType = typeof(Localization.Validation), ErrorMessageResourceName = "EmailAddressInvalidFormat")]
         public string Email { get; set; }
     }
 }
